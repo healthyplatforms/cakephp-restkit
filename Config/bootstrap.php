7364800b77a8657d56f74749bea2c6a948210aa9
bravo-kernel <rob@vanerkel.net>
2012-06-21 14:31:16 +0200

    Adding comment motivating the Configure::load() in bootstrap.php

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
+// We need to load the config file here because initializing it from the App's bootstrap.php
+// using CakePlugin::load(array('RestKit' => array('bootstrap' => true))
+// will only do a require() and not a load() of the configfile making the settings
+// unavailable for use inside the plugin.
 Configure::load('RestKit.config');

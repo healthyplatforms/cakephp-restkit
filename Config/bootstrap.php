ef45dbc7617f8f02b4816c6eb9a33d989119cfb9
bravo-kernel <rob@vanerkel.net>
2012-06-22 12:43:13 +0200

    Adding RestKitExceptionHandler to bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,7 +1,14 @@
 <?php
 
-// We need to load the config file here because initializing it from the App's bootstrap.php
+// We need to load our config file here because initializing it from the App's bootstrap.php
 // using CakePlugin::load(array('RestKit' => array('bootstrap' => true))
-// will only do a require() and not a load() of the configfile making the settings
-// unavailable for use inside the plugin.
+// would only do a require() and not a load() making the settings unavailable
+// for use inside the plugin.
 Configure::load('RestKit.config');
+
+// We override the default ExceptionHandler with our own RestKitExceptionHandler here
+Configure::write('Exception', array(
+	'handler' => 'ErrorHandler::handleException',
+	'renderer' => 'RestKit.RestKitExceptionRenderer',
+	'log' => true
+));

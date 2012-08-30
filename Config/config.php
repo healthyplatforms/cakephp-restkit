94fbcf2a3ece3630772117061f332ad15800f447
bravo-kernel <rob@vanerkel.net>
2012-08-30 18:07:20 +0200

    Preparing config file for Authentication

--- Config/config.php
+++ Config/config.php
@@ -1,16 +1,16 @@
 <?php
 
 $config['RestKit'] = array(
-
+    'version' => '0.0.1',
     'Request' => array(
-	'enabledExtensions' => array('xml', 'json'),
-	'prefix' => 'v1',			// prefix string to enable, false to disable
-	'forcePrefix' => false			// true will allow access through the prefix path only (disables default CakePHP routes)
-	),
-
-    'version' => '1.0.0',
+	'enabledExtensions' => array('xml', 'json'),	// choose what output your API will support
+	'prefix' => 'v1',				// prefix string to enable, false to disable
+	'forcePrefix' => false				// true will allow access through the prefix path only (disables default CakePHP routes)
+    ),
+    'Authentication' => array(
+	'enabledMethods' => array('basic', 'oauth2'),	// choose to support basic, digest, apikey, oauth or oauth2
+    ),
     'enableOptionValidation' => true,
-
     // Change the default CakePHP status codes (commented ones) or add your own
     'statusCodes' => array(
 //		100 => 'Continue',
@@ -47,12 +47,12 @@ $config['RestKit'] = array(
 //		415 => 'Unsupported Media Type',
 //		416 => 'Requested range not satisfiable',
 //		417 => 'Expectation Failed',
-		429 => 'Too Many Requests',
+	429 => 'Too Many Requests',
 //		500 => 'Internal Server Error',
 //		501 => 'Not Implemented',
 //		502 => 'Bad Gateway',
 //		503 => 'Service Unavailable',
 //		504 => 'Gateway Time-out',
-		666 => 'Something very evil',
+	666 => 'Something very evil',
     )
 );

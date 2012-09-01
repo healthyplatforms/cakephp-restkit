b4d1010e6fa50299b5d6c4faf974b3ecab8802cf
bravo-kernel <rob@vanerkel.net>
2012-09-01 14:49:57 +0200

    Adding draft HTTP Status Codes to config.php

--- Config/config.php
+++ Config/config.php
@@ -2,18 +2,22 @@
 
 $config['RestKit'] = array(
 
+    'version' => '1.0.0',
+
     'Request' => array(
 	'enabledExtensions' => array('xml', 'json'),
-	'prefix' => 'v1',			// prefix string to enable, false to disable
-	'forcePrefix' => false			// true will allow access through the prefix path only (disables default CakePHP routes)
-	),
+	'prefix' => 'v1', // prefix string to enable, false to disable
+	'forcePrefix' => false   // true will allow access through the prefix path only (disables default CakePHP routes)
+    ),
 
-    'version' => '1.0.0',
     'enableOptionValidation' => true,
 
-    // Override default CakePHP HTTP Status Codes or add custom ones
+    // Override or expand default CakePHP HTTP Status Codes
     'statusCodes' => array(
-		429 => 'Too Many Requests',
-		666 => 'Something Very Evil',
+	428 => 'Precondition Required',			// proposed draft
+	429 => 'Too Many Requests',			// proposed draft
+	431 => 'Request Header Fields Too Large',	// proposed draft
+	511 => 'Network Authentication Required',	// proposed draft
+	666 => 'Something Very Evil',			// a completely custom status code
     )
 );
